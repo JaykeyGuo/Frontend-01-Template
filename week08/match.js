8ec3095d8a8e5ccd7d71be2518144d4f9ca4f91b
Jaykey.Guo <jkland927@gmail.com>
2020-06-03 16:58:32 +0800

    add week08 homework and note

---
+++ week08/match.js
@@ -0,0 +1,24 @@
+function match(element, selector) {
+  if (!selector || !element.attributes) {
+    return false;
+  }
+
+  if (selector.charAt(0) === '#') {
+    var attr = element.attributes.filter(attr => attr.name === 'id')[0];
+    if (attr && attr.value === selector.replace('#', '')) {
+      return true;
+    }
+  } else if (selector.charAt(0) === '.') {
+    var attr = element.attributes.filter(attr => attr.name === 'class')[0];
+    if (attr && attr.value === selector.replace('.', '')) {
+      return true;
+    }
+  } else {
+    if (element.tagName === selector) {
+      return true;
+    }
+  }
+  return false;
+}
+
+match('div #id.class', document.getElementById('id'));
